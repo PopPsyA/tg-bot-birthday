fdd356625d9392311fc547c8e7a1c28995b62a64
Devit951 <devit951@gmail.com>
2020-06-10 00:13:27 +0300

    SimpleDateFormatter to DateTimeFormatter

--- src/main/kotlin/handler/birthdaychecker/BirthdayCheckerHandler.kt
+++ src/main/kotlin/handler/birthdaychecker/BirthdayCheckerHandler.kt
@@ -5,6 +5,7 @@ import common.randomBirthdayTemplate
 import common.sendMessage
 import database.TelegramUser
 import handler.TelegramHandler
+import logDebug
 import me.ivmg.telegram.Bot
 import me.ivmg.telegram.HandleUpdate
 import me.ivmg.telegram.dispatcher.handlers.Handler
@@ -13,7 +14,9 @@ import org.jetbrains.exposed.sql.selectAll
 import org.jetbrains.exposed.sql.transactions.transaction
 import java.text.SimpleDateFormat
 import java.time.LocalDate
+import java.time.LocalDateTime
 import java.time.ZoneId
+import java.time.format.DateTimeFormatter
 import java.util.*
 import kotlin.random.Random
 
@@ -23,27 +26,31 @@ class BirthdayCheckerHandler: Handler(BirthdayHandlerUpdate()), TelegramHandler 
 
     override val groupIdentifier: String = "BirthdayHandler"
 
-    override fun checkUpdate(update: Update) = everyDay()
+    override fun checkUpdate(update: Update): Boolean {
+        logDebug("checkupdate ${update.message?.chat?.id}")
+        return everyDay()
+    }
 
-    private fun everyDay() = Calendar.getInstance().get(Calendar.HOUR_OF_DAY) == 0
+    private fun everyDay() = true
 
     private class BirthdayHandlerUpdate: HandleUpdate{
-        private val sdf = SimpleDateFormat("dd.MM.yyyy")
+        private val dtf = DateTimeFormatter.ofPattern("dd.MM.yyyy")
 
         override fun invoke(bot: Bot, update: Update) {
-            bot.sendMessage(update, "С днем рожденья!")
             transaction {
                 TelegramUser
                     .selectAll()
                     .forEach {
+                        bot.sendMessage(update, "С днем рожденья!")
                         it[TelegramUser.dateOfBirth]?.let { dateOfBirth ->
-                            val parsedDateOfBirth = sdf.parse(dateOfBirth).toInstant().atZone(ZoneId.systemDefault()).toLocalDate()
+                            val parsedDateOfBirth = LocalDate.parse(dateOfBirth, dtf)
                             val currentDate = LocalDate.now()
                             if (currentDate.monthValue == parsedDateOfBirth.monthValue
                                 && currentDate.dayOfMonth == parsedDateOfBirth.dayOfMonth){
                                 bot.sendMessage(update,
                                     randomBirthdayTemplate()
                                 )
+                                bot.sendMessage(chatId = it[TelegramUser.id], text = randomBirthdayTemplate())
                             }
                         }
                     }
