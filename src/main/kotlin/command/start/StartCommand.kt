150c77be4d4b1d999095fa2f9cda0f964d154693
Devit951 <devit951@gmail.com>
2020-06-09 22:20:10 +0300

    Log languageCode

--- src/main/kotlin/command/start/StartCommand.kt
+++ src/main/kotlin/command/start/StartCommand.kt
@@ -4,14 +4,18 @@ import command.AbstractTelegramCommand
 import me.ivmg.telegram.HandleUpdate
 import me.ivmg.telegram.entities.BotCommand
 
+import common.sendMessage
+import logDebug
+
 class StartCommand: AbstractTelegramCommand() {
 
     override fun handlerUpdate(): HandleUpdate = { bot, update ->
         val message = update.message!!
         bot.sendMessage(
-            chatId = message.chat.id,
-            text = "Привет ${message.from?.firstName.orEmpty()} ${message.from?.lastName.orEmpty()}"
+            update,
+            msg = "Привет ${message.from?.firstName.orEmpty()} ${message.from?.lastName.orEmpty()}"
         )
+        logDebug("Start with languageCode = ${message.from?.languageCode}")
     }
 
     override fun botCommand() = BotCommand(
